6b52b2b28a0eeadf49c5fc2fecf176aabd573795
Md Akhtar <akhtar.niet11@gmail.com>
2025-11-22 11:43:12 +0530

    Add project files.

---
+++ PracticeForGit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticeForGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
